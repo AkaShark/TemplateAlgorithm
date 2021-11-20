70755819f330452ceeebc9296bb21c0d7a02df62
Sharker <1548742234@qq.com>
2021-11-20 23:50:33 +0800

    feat: 面试复习

---
+++ src/Interview/Offer_07/Solution.java
@@ -0,0 +1,19 @@
+package Interview.Offer_07;
+
+import Util.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    HashMap<Integer, Integer> indexMap = new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            
+        }
+    }
+
+    private TreeNode buildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+
+    }
+}
