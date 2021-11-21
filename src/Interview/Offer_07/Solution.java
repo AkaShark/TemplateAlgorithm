99d6a20ecf4f6d8ac1a69e22a05dcfb100765452
Sharker <1548742234@qq.com>
2021-11-21 11:34:43 +0800

    feat: 日常

--- src/Interview/Offer_07/Solution.java
+++ src/Interview/Offer_07/Solution.java
@@ -6,14 +6,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-    HashMap<Integer, Integer> indexMap = new HashMap<>();
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
+    static private HashMap<Integer, Integer> indexMap = new HashMap<>();
+    static public TreeNode buildTree(int[] preorder, int[] inorder) {
         for (int i = 0; i < inorder.length; i++) {
-            
+            indexMap.put(inorder[i], i);
         }
+        return build(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
     }
 
-    private TreeNode buildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+   static private TreeNode build(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) return null;
+        // 获取根节点的值
+        int rootValue = preorder[preorder_left];
+        // 计算左子树节点个数
+        int leftSubTreeLen = indexMap.get(rootValue) - inorder_left;
+        // 创建根节点
+        TreeNode root = new TreeNode(rootValue);
+        // 递归
+        root.left = build(preorder, inorder, preorder_left + 1, preorder_left + leftSubTreeLen, inorder_left, indexMap.get(preorder[preorder_left]) - 1);
+        root.right = build(preorder, inorder, preorder_left + leftSubTreeLen + 1, preorder_right, indexMap.get(preorder[preorder_left]) + 1, inorder_right);
+        return root;
+    }
 
+    public static void main(String[] args) {
+        buildTree(new int[]{3,9,20,15,7}, new int[]{9,3,15,20,7});
     }
 }
