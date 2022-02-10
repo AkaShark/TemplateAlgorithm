ceab2e56b15869b4a8e36e51b12d3817addabf6e
Sharker <1548742234@qq.com>
2022-02-10 21:37:42 +0800

    LeetCode_21 补充递归方法

--- src/labuladong_Algorithm/SignleNodeList/LeetCode_21/Solution.java
+++ src/labuladong_Algorithm/SignleNodeList/LeetCode_21/Solution.java
@@ -3,7 +3,8 @@ package labuladong_Algorithm.SignleNodeList.LeetCode_21;
 import Util.ListNode;
 
 public class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    // 非递归
+    public ListNode mergeTwoLists_1(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1);
         ListNode cur = dummy;
         while (list1 != null && list2 != null) {
@@ -24,4 +25,19 @@ public class Solution {
         }
         return dummy.next;
     }
+    // 递归
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        else if (list2 == null) return list1;
+        else {
+            if (list1.val > list2.val) {
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            } else {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            }
+        }
+    }
+
 }
