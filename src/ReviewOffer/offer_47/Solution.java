cbc26a2effa3cbf87219f4e0eaaab16d9d6d89b1
Sharker <1548742234@qq.com>
2022-07-13 15:07:38 +0800

    feat: offer_47 礼物的最大价值

--- src/ReviewOffer/offer_47/Solution.java
+++ src/ReviewOffer/offer_47/Solution.java
@@ -1,4 +1,24 @@
 package ReviewOffer.offer_47;
 
 public class Solution {
+    public int maxValue(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        int rows = grid.length, cols = grid[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == 0 && j == 0) {
+                    dp[0][0] = grid[0][0];
+                    continue;
+                }
+                if (i == 0) { // j != 0
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+        return dp[rows - 1][cols - 1];
+    }
 }
