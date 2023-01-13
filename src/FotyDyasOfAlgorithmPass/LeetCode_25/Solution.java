64304097729c7785a89f700f2e20437a99729ab7
Sharker <1548742234@qq.com>
2023-01-13 23:48:00 +0800

    chore: 算法通关40讲链表 k哥一组翻转

--- src/FotyDyasOfAlgorithmPass/LeetCode_25/Solution.java
+++ src/FotyDyasOfAlgorithmPass/LeetCode_25/Solution.java
@@ -1,5 +1,41 @@
 package FotyDyasOfAlgorithmPass.LeetCode_25;
 
-public class Solution {
+import FotyDyasOfAlgorithmPass.Utils.ListNode;
 
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while (head != null) {
+            ListNode tail = pre;
+            for (int i = 0; i < k; i++) {
+                tail = tail.next;
+                if (tail == null) {
+                    return dummy.next;
+                }
+            }
+            ListNode next = tail.next;
+            ListNode[] reversedLists = reverse(pre, tail);
+            head = reversedLists[0];
+            tail = reversedLists[1];
+            pre.next = head;
+            tail.next = next;
+            pre = tail;
+            head = next;
+
+        }
+        return dummy.next;
+    }
+
+    private ListNode[] reverse(ListNode head, ListNode tail) {
+        ListNode pre = tail.next;
+        while (pre != tail) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return new ListNode[]{tail, head};
+    }
 }
