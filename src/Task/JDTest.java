84f00e928ae3385e9ce346e3d4935468f8546a5c
Sharker <1548742234@qq.com>
2023-03-02 13:38:07 +0800

    chore:  JD 算法题目

--- src/Task/JDTest.java
+++ src/Task/JDTest.java
@@ -1,2 +1,27 @@
-package Task;public class JDTest {
+package Task;
+
+import java.util.Map;
+
+/*
+* 有一栋100层高的大楼，给你两个完全相同的玻璃球，假设从某一层开始丢下玻璃球会摔碎，怎么利用手中的两个玻璃球，用什么最优策略（最坏情况下最少次数）知道这个临界的层是第几层*/
+/*
+* 算法题：
+
+给定字典中的两个词，长度相等。写一个方法，把一个词转换成另一个词， 但是一次只能改变一个字符。每一步得到的新词都必须能在字典中找到。
+
+编写一个程序，返回一个可能的转换序列。如有多个可能的转换序列，你可以返回任何一个。
+
+例如：
+输入:
+beginWord = "hit",
+endWord = "cog",
+wordList = ["hot","dot","dog","lot","log","cog"]
+
+输出:
+["hit","hot","dot","lot","log","cog"]
+*/
+class JDTest {
+    public static void main(String[] args) {
+        
+    }
 }
